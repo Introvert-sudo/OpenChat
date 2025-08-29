a1751ecc9ee5cc86c7298b27294b9926ee97b1dd
Introvert <yintrovert.dev@gmail.com>
2025-08-29 19:57:40 +0300

    Add files via upload

---
+++ AyaChatApp/src/main/java/com/AyaChatApp/AyaChatApp/AyaChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.AyaChatApp.AyaChatApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyaChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AyaChatAppApplication.class, args);
+	}
+
+}
---
+++ AyaChatApp/src/test/java/com/AyaChatApp/AyaChatApp/AyaChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AyaChatApp.AyaChatApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AyaChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
